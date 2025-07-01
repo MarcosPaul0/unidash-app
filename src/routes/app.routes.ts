03cb3fb225012373cfcdcb64aec64d63026c4b31
MarcosPaul0 <marcosphip7@gmail.com>
2025-07-01 20:33:35 -0300

    feat: adiciona rotas para novas entidades

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -1,7 +1,15 @@
 export const APP_ROUTES = {
   private: {
     dashboard: "/dashboard",
-    courses: "/courses",
+    course: "/course/list",
+    editCourse: "/course/edit",
+    registerCourse: "/course/register",
+    student: "/student/list",
+    editStudent: "/student/edit",
+    registerStudent: "/student/register",
+    teacher: "/teacher/list",
+    editTeacher: "/teacher/edit",
+    registerTeacher: "/teacher/register",
   },
   public: {
     login: "/login",
