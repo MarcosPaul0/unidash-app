c3b770c089c517a4a31374388249e88de871985c
MarcosPaul0 <marcosphip7@gmail.com>
2025-07-08 16:48:46 -0300

    feat: adiciona novas rotas

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -1,15 +1,63 @@
 export const APP_ROUTES = {
   private: {
     dashboard: "/dashboard",
+
     course: "/course/list",
     editCourse: "/course/edit",
     registerCourse: "/course/register",
+
     student: "/student/list",
     editStudent: "/student/edit",
     registerStudent: "/student/register",
+
     teacher: "/teacher/list",
     editTeacher: "/teacher/edit",
     registerTeacher: "/teacher/register",
+
+    indicator: "/indicator",
+
+    courseIndicator: "/indicator/courseIndicator",
+    registerCourseIndicator: "/indicator/courseIndicator/register",
+
+    courseComplementaryActivitiesExtensionIndicator:
+      "/indicator/courseComplementaryActivitiesExtensionIndicator",
+    registerCourseComplementaryActivitiesExtensionIndicator:
+      "/indicator/courseComplementaryActivitiesExtensionIndicator/register",
+
+    courseComplementaryActivitiesSearchIndicator:
+      "/indicator/courseComplementaryActivitiesSearchIndicator",
+    registerCourseComplementaryActivitiesSearchIndicator:
+      "/indicator/courseComplementaryActivitiesSearchIndicator/register",
+
+    courseComplementaryActivitiesTeachingIndicator:
+      "/indicator/courseComplementaryActivitiesTeachingIndicator",
+    registerCourseComplementaryActivitiesTeachingIndicator:
+      "/indicator/courseComplementaryActivitiesTeachingIndicator/register",
+
+    courseCompletionWorkIndicator: "/indicator/courseCompletionWorkIndicator",
+    registerCourseCompletionWorkIndicator:
+      "/indicator/courseCompletionWorkIndicator/register",
+
+    courseCoordinationIndicator: "/indicator/courseCoordinationIndicator",
+    registerCourseCoordinationIndicator:
+      "/indicator/courseCoordinationIndicator/register",
+
+    courseExtensionActivitiesIndicator:
+      "/indicator/courseExtensionActivitiesIndicator",
+    registerCourseExtensionActivitiesIndicator:
+      "/indicator/courseExtensionActivitiesIndicator/register",
+
+    courseInternshipIndicator: "/indicator/courseInternshipIndicator",
+    registerCourseInternshipIndicator:
+      "/indicator/courseInternshipIndicator/register",
+
+    studentEgressIndicator: "/indicator/studentEgressIndicator",
+    registerStudentEgressIndicator:
+      "/indicator/studentEgressIndicator/register",
+
+    studentIngressIndicator: "/indicator/studentIngressIndicator",
+    registerStudentIngressIndicator:
+      "/indicator/studentIngressIndicator/register",
   },
   public: {
     login: "/login",
