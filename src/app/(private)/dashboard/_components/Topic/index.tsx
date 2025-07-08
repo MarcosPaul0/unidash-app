25ad9c7bfb178a6548b941cbfa17a1652239c893
MarcosPaul0 <marcosphip7@gmail.com>
2025-07-08 16:49:48 -0300

    refactor: atualiza estilo de tópico

--- src/app/(private)/dashboard/_components/Topic/index.tsx
+++ src/app/(private)/dashboard/_components/Topic/index.tsx
@@ -2,5 +2,5 @@ import { Badge } from "@unidash/components/Badge";
 import { TopicProps } from "./topicProps.interface";
 
 export function Topic({ title }: TopicProps) {
-  return <Badge variant="outline">{title}</Badge>;
+  return <Badge>{title}</Badge>;
 }
