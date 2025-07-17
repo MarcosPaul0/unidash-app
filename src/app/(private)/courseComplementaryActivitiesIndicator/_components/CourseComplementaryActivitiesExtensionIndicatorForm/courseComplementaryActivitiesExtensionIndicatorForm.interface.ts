fc33e830167308fecb58c232951489c43dbb9194
MarcosPaul0 <marcosphip7@gmail.com>
2025-07-17 12:10:24 -0300

    feat: adiciona interface para formulário de atividades complementares de extensão

--- src/app/(private)/courseComplementaryActivitiesIndicator/_components/CourseComplementaryActivitiesExtensionIndicatorForm/courseComplementaryActivitiesExtensionIndicatorForm.interface.ts
+++ src/app/(private)/courseComplementaryActivitiesIndicator/_components/CourseComplementaryActivitiesExtensionIndicatorForm/courseComplementaryActivitiesExtensionIndicatorForm.interface.ts
@@ -1,3 +1,13 @@
 export interface CourseComplementaryActivitiesExtensionIndicatorFormProps {
   title: string;
+  handleNextStep: () => void;
+  handlePreviousStep: () => void;
+}
+
+export interface CourseComplementaryActivitiesExtensionIndicatorFormData {
+  culturalActivities: number;
+  sportsCompetitions: number;
+  awardsAtEvents: number;
+  studentRepresentation: number;
+  participationInCollegiateBodies: number;
 }
--- src/app/(private)/courseComplementaryActivitiesIndicator/_components/CourseComplementaryActivitiesExtensionIndicatorForm/index.tsx
+++ src/app/(private)/courseComplementaryActivitiesIndicator/_components/CourseComplementaryActivitiesExtensionIndicatorForm/index.tsx
@@ -1,6 +1,7 @@
 import {
   Card,
   CardContent,
+  CardFooter,
   CardHeader,
   CardSubtitle,
   CardTitle,
@@ -8,93 +9,118 @@ import {
 import { useFormContext } from "react-hook-form";
 import { FormInput } from "@unidash/components/FormInput";
 import { Button } from "@unidash/components/Button";
-import { PaperPlaneTiltIcon } from "@phosphor-icons/react/dist/ssr";
-import { CourseComplementaryActivitiesExtensionIndicatorFormProps } from "./courseComplementaryActivitiesExtensionIndicatorForm.interface";
+import {
+  ArrowArcLeftIcon,
+  ArrowRightIcon,
+} from "@phosphor-icons/react/dist/ssr";
+import {
+  CourseComplementaryActivitiesExtensionIndicatorFormData,
+  CourseComplementaryActivitiesExtensionIndicatorFormProps,
+} from "./courseComplementaryActivitiesExtensionIndicatorForm.interface";
 import { FormFile } from "@unidash/components/FormFile";
+import { FormEvent } from "react";
 
 export function CourseComplementaryActivitiesExtensionIndicatorForm({
   title,
+  handleNextStep,
+  handlePreviousStep,
 }: CourseComplementaryActivitiesExtensionIndicatorFormProps) {
-  const { control } = useFormContext();
+  const { control, trigger } =
+    useFormContext<CourseComplementaryActivitiesExtensionIndicatorFormData>();
+
+  async function sendCourseComplementaryActivitiesExtensionIndicator(
+    event: FormEvent
+  ) {
+    event.preventDefault();
+
+    const isValid = await trigger([
+      "awardsAtEvents",
+      "culturalActivities",
+      "participationInCollegiateBodies",
+      "sportsCompetitions",
+      "studentRepresentation",
+    ]);
+
+    if (isValid) {
+      handleNextStep();
+    }
+  }
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
-      </CardHeader>
+    <form onSubmit={sendCourseComplementaryActivitiesExtensionIndicator}>
+      <Card>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+        </CardHeader>
 
-      <CardContent className="flex flex-col gap-8">
-        <FormFile
-          label="Arraste e solte a planilha de indicadores"
-          control={control}
-          name="spreadsheet"
-        />
+        <CardContent className="flex flex-col gap-8">
+          <FormFile
+            label="Arraste e solte a planilha de indicadores"
+            control={control}
+            name="spreadsheet"
+          />
 
-        <CardSubtitle>Período de referência</CardSubtitle>
+          <CardSubtitle>Atividades complementares de extensão</CardSubtitle>
 
-        <div className="flex gap-8 w-full">
-          <FormInput
-            control={control}
-            name="year"
-            placeholder="Informe o ano de referência"
-            label="Ano de referência"
-          />
-          <FormInput
-            control={control}
-            name="semester"
-            placeholder="Informe o semestre de referência"
-            label="Semestre"
-          />
-        </div>
+          <div className="flex gap-8 w-full">
+            <FormInput
+              control={control}
+              type="number"
+              name="culturalActivities"
+              placeholder="Quantidade de atividades culturais, socialização e integração"
+              label="Atividades culturais, socialização e integração"
+            />
+            <FormInput
+              control={control}
+              type="number"
+              name="sportsCompetitions"
+              placeholder="Quantidade de participação em competições esportivas"
+              label="Participação em competições esportivas"
+            />
+            <FormInput
+              control={control}
+              name="awardsAtEvents"
+              type="number"
+              placeholder="Quantidade de premiação em eventos representando a UNIFEI"
+              label="Premiação em eventos representando a UNIFEI"
+            />
+          </div>
 
-        <CardSubtitle>Atividades complementares de extensão</CardSubtitle>
+          <div className="flex gap-8 w-full">
+            <FormInput
+              control={control}
+              type="number"
+              name="studentRepresentation"
+              placeholder="Quantidade de representação estudantil"
+              label="Representação estudantil"
+            />
+            <FormInput
+              control={control}
+              type="number"
+              name="participationInCollegiateBodies"
+              placeholder="Quantidade de participação em órgãos colegiados"
+              label="Participação em órgãos colegiados"
+            />
+          </div>
+        </CardContent>
 
-        <div className="flex gap-8 w-full">
-          <FormInput
-            control={control}
-            type="number"
-            name="culturalActivities"
-            placeholder="Quantidade de atividades culturais, socialização e integração"
-            label="Atividades culturais, socialização e integração"
-          />
-          <FormInput
-            control={control}
-            type="number"
-            name="sportsCompetitions"
-            placeholder="Quantidade de participação em competições esportivas"
-            label="Participação em competições esportivas"
-          />
-          <FormInput
-            control={control}
-            name="awardsAtEvents"
-            type="number"
-            placeholder="Quantidade de premiação em eventos representando a UNIFEI"
-            label="Premiação em eventos representando a UNIFEI"
-          />
-        </div>
+        <CardFooter className="gap-6">
+          <Button
+            className="w-full max-w-80"
+            size="lg"
+            type="button"
+            onClick={handlePreviousStep}
+          >
+            <ArrowArcLeftIcon />
+            Anterior
+          </Button>
 
-        <div className="flex gap-8 w-full">
-          <FormInput
-            control={control}
-            type="number"
-            name="studentDDRepresentation"
-            placeholder="Quantidade de representação estudantil"
-            label="Representação estudantil"
-          />
-          <FormInput
-            control={control}
-            type="number"
-            name="participationInCollegiateBodies"
-            placeholder="Quantidade de participação em órgãos colegiados"
-            label="Participação em órgãos colegiados"
-          />
-        </div>
-
-        <Button className="max-w-56" size="lg">
-          <PaperPlaneTiltIcon />
-          Enviar
-        </Button>
-      </CardContent>
-    </Card>
+          <Button className="w-full max-w-80" size="lg">
+            Próximo
+            <ArrowRightIcon />
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
   );
 }
