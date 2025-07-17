fa8a3e3770c9f0cacc926553a4d85fdeb79bd718
MarcosPaul0 <marcosphip7@gmail.com>
2025-07-17 12:11:19 -0300

    refactor: atualiza página de registro de indicadores de atividades complementares

--- src/app/(private)/courseComplementaryActivitiesIndicator/register/page.tsx
+++ src/app/(private)/courseComplementaryActivitiesIndicator/register/page.tsx
@@ -11,10 +11,54 @@ import {
   BreadcrumbSeparator,
 } from "@unidash/components/Breadcrumb";
 import { APP_ROUTES } from "@unidash/routes/app.routes";
+import { useCallback, useState } from "react";
+import { CourseComplementaryActivitiesSearchIndicatorForm } from "../_components/CourseComplementaryActivitiesSearchIndicatorForm";
+import { CourseComplementaryActivitiesTeachingIndicatorForm } from "../_components/CourseComplementaryActivitiesTeachingIndicatorForm";
+import { CourseComplementaryActivitiesIndicatorFormData } from "../_components/CourseComplementaryActivitiesTeachingIndicatorForm/courseComplementaryActivitiesTeachingIndicatorForm.interface";
 import { CourseComplementaryActivitiesExtensionIndicatorForm } from "../_components/CourseComplementaryActivitiesExtensionIndicatorForm";
 
-export default function RegisterCourseComplementaryActivitiesExtensionIndicatorPage() {
-  const formMethods = useForm();
+export default function RegisterCourseComplementaryActivitiesIndicatorPage() {
+  const [formStep, setFormStep] = useState(0);
+
+  const formMethods = useForm<CourseComplementaryActivitiesIndicatorFormData>();
+
+  const nextStep = useCallback(() => {
+    setFormStep((currentStep) => {
+      if (currentStep === 2) {
+        return currentStep;
+      }
+
+      return currentStep + 1;
+    });
+  }, []);
+
+  const previousStep = useCallback(() => {
+    setFormStep((currentStep) => {
+      if (currentStep === 0) {
+        return currentStep;
+      }
+
+      return currentStep - 1;
+    });
+  }, []);
+
+  const formSteps = [
+    <CourseComplementaryActivitiesSearchIndicatorForm
+      key="CourseComplementaryActivitiesSearchIndicatorForm"
+      title="Adicionar dados de indicadores de atividades complementares de pesquisa"
+      handleNextStep={nextStep}
+    />,
+    <CourseComplementaryActivitiesExtensionIndicatorForm
+      key="CourseComplementaryActivitiesExtensionIndicatorForm"
+      title="Adicionar dados de indicadores de atividades complementares de extensão"
+      handleNextStep={nextStep}
+      handlePreviousStep={previousStep}
+    />,
+    <CourseComplementaryActivitiesTeachingIndicatorForm
+      key="CourseComplementaryActivitiesTeachingIndicatorForm"
+      title="Adicionar dados de indicadores de atividades complementares de ensino"
+    />,
+  ];
 
   return (
     <>
@@ -40,9 +84,7 @@ export default function RegisterCourseComplementaryActivitiesExtensionIndicatorP
         </Breadcrumb>
       </Toolbar>
 
-      <FormProvider {...formMethods}>
-        <CourseComplementaryActivitiesExtensionIndicatorForm title="Adicionar dados de indicadores de atividades complementares de extensão" />
-      </FormProvider>
+      <FormProvider {...formMethods}>{formSteps[formStep]}</FormProvider>
     </>
   );
 }
