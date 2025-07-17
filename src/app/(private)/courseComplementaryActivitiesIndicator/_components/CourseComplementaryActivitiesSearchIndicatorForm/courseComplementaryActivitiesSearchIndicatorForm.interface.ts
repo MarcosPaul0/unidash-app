7a9966a937e9120ea0afe1ff5e1a04b23b7c383b
MarcosPaul0 <marcosphip7@gmail.com>
2025-07-17 12:10:37 -0300

    feat: adiciona interface para formulário de atividades complementares de pesquisa

--- src/app/(private)/courseComplementaryActivitiesIndicator/_components/CourseComplementaryActivitiesSearchIndicatorForm/courseComplementaryActivitiesSearchIndicatorForm.interface.ts
+++ src/app/(private)/courseComplementaryActivitiesIndicator/_components/CourseComplementaryActivitiesSearchIndicatorForm/courseComplementaryActivitiesSearchIndicatorForm.interface.ts
@@ -1,3 +1,18 @@
+import { Semester } from "@unidash/interfaces/apiResponses/courseCoordinationIndicatorApiResponse.interface";
+
 export interface CourseComplementaryActivitiesSearchIndicatorFormProps {
   title: string;
+  handleNextStep: () => void;
+}
+
+export interface CourseComplementaryActivitiesSearchIndicatorFormData {
+  year: number;
+  semester: Semester;
+  scientificInitiation: number;
+  developmentInitiation: number;
+  publishedArticles: number;
+  fullPublishedArticles: number;
+  publishedAbstracts: number;
+  presentationOfWork: number;
+  participationInEvents: number;
 }
