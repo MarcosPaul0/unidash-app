c3acd0e1d2aa4172ea21944602c8ca403b0f6022
MarcosPaul0 <marcosphip7@gmail.com>
2025-10-06 20:22:04 -0300

    refactor: update charts skeletons

--- src/app/(private)/dashboard/_charts/activities/ActiviteiesSkeletons/index.tsx
+++ src/app/(private)/dashboard/_charts/activities/ActiviteiesSkeletons/index.tsx
@@ -3,22 +3,12 @@ import { Skeleton } from "@unidash/components/Skeleton";
 export function ActivitiesSkeletons() {
   return (
     <>
-      {/* <div className="grid grid-cols-4 gap-8">
-        <Skeleton className="w-ful h-[184px]" />
-        <Skeleton className="w-ful h-[184px]" />
-        <Skeleton className="w-ful h-[184px]" />
-        <Skeleton className="w-ful h-[184px]" />
-      </div> */}
-
       <Skeleton className="w-ful h-[46px] max-w-[300px] rounded-xl" />
 
-      <Skeleton className="w-ful h-[568px]" />
-      {/* <Skeleton className="w-ful h-[568px]" />
-      <Skeleton className="w-ful h-[568px]" />
-
-      <Skeleton className="w-ful h-[46px] max-w-[300px] rounded-xl" />
-
-      <Skeleton className="w-ful h-[568px]" /> */}
+      <div className="flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-8">
+        <Skeleton className="w-ful h-[568px]" />
+        <Skeleton className="w-ful h-[568px]" />
+      </div>
     </>
   );
 }
--- src/app/(private)/dashboard/_charts/courseCompletionWork/CompletionWorkSkeletons/index.tsx
+++ src/app/(private)/dashboard/_charts/courseCompletionWork/CompletionWorkSkeletons/index.tsx
@@ -5,10 +5,7 @@ export function CompletionWorkSkeletons() {
     <>
       <Skeleton className="w-ful h-[46px] max-w-[300px] rounded-xl" />
 
-      <div className="flex flex-col gap-4 md:grid md:grid-cols-7 md:gap-8">
-        <Skeleton className="w-ful h-[596px] col-span-3" />
-        <Skeleton className="w-ful h-[596px] col-span-4" />
-      </div>
+      <Skeleton className="w-ful h-[596px]" />
     </>
   );
 }
